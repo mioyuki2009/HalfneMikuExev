6b31503af12a440e7ecbb43c41244721ecf0dc8b
mioyuki2009 <mioyuki2009@gmail.com>
2023-04-10 16:36:04 +0800

    third party

---
+++ make.bat
@@ -0,0 +1,1 @@
+call vendor\premake-5.0.0-beta2-windows\premake5.exe --file=premake5.lua vs2022
---
+++ premake5.lua
@@ -0,0 +1,26 @@
+workspace "HalfneMikuExev"
+   configurations { "Debug", "Release" }
+   platforms {"Win64"}
+
+project "HalfneMikuExev"
+   kind "ConsoleApp"
+   language "C++"
+   targetdir "bin/%{cfg.buildcfg}"
+   local SOURCEPATH = "source"
+   local IMGUIPATH = "vendor/imgui"
+   local WINDOWSBACKENDSPATH = IMGUIPATH.."/backends/imgui_impl_win32.*"
+   local DX12BACKENDSPATH = IMGUIPATH.."/backends/imgui_impl_dx12.*"
+
+   files { IMGUIPATH.."/imgui*.h", IMGUIPATH.."/imgui*.cpp", WINDOWSBACKENDSPATH, DX12BACKENDSPATH ,SOURCEPATH.."/main.cpp"}
+
+   includedirs {IMGUIPATH, IMGUIPATH.."/backends"}
+   
+   links {"d3d12", "dxgi"}
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
